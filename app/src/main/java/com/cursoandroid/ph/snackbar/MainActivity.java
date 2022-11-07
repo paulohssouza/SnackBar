8f83213fdd0e441b0c5dd03d1cbb145e782ae9af
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2022-11-07 17:01:59 -0300

    Exercise use snackbar

---
+++ app/src/main/java/com/cursoandroid/ph/snackbar/MainActivity.java
@@ -0,0 +1,36 @@
+package com.cursoandroid.ph.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonOpen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonOpen = findViewById(R.id.buttonOpen);
+        buttonOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(
+                        view,
+                        "Botão pressionado",
+                        Snackbar.LENGTH_INDEFINITE
+                ).setAction("Confirmar", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                buttonOpen.setText("Botão abrir alterado");
+                            }
+                        }).show();
+            }
+        });
+    }
+}
